30266a87a0acff8719f2c85d561b0f32dd37ce5a
CODEMCD <psbum1609@gmail.com>
2020-05-16 14:02:12 +0900

    #3.2 Making a JS Clock part Two

--- clone-momentum/clock.js
+++ clone-momentum/clock.js
@@ -4,13 +4,17 @@ const clockContainer = document.querySelector(".js-clock"),
 function getTime() {
     const date = new Date();
     const minutes = date.getMinutes();
-    const hours = date.getHours() ;
+    const hours = date.getHours();
+    const seconds = date.getSeconds();
 
-    clockTitle.innerText = `${hours}:${minutes}`;
+    clockTitle.innerText = `${
+        hours < 10 ? `0${hours}` : hours}:${
+            minutes < 10 ? `0${minutes}` : minutes}:${
+                seconds < 10 ? `0${seconds}` : seconds}`;
 }
 
 function init() {
-    getTime();
+    setInterval(getTime, 1000);
 }
 
 init();
